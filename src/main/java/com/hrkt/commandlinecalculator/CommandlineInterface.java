f465fd9f4e0d810a92808341872ddb70b7d6e5c9
hrkt <hiroki110@gmail.com>
2019-12-04 07:43:44 +0900

    bump jline2 to 3

--- src/main/java/com/hrkt/commandlinecalculator/CommandlineInterface.java
+++ src/main/java/com/hrkt/commandlinecalculator/CommandlineInterface.java
@@ -1,10 +1,10 @@
 package com.hrkt.commandlinecalculator;
 
-import jline.console.ConsoleReader;
 import lombok.extern.slf4j.Slf4j;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 import org.springframework.stereotype.Component;
 
-import javax.xml.transform.sax.SAXSource;
 import java.io.IOException;
 
 @Component
@@ -13,16 +13,14 @@ public class CommandlineInterface {
     public void run(String... args) {
         log.info("hello");
 
-        ConsoleReader cr = null;
-        try {
-            cr = new ConsoleReader();
+        try(Terminal terminal = TerminalBuilder.terminal()) {
             int ch = 0;
-            while ((ch = cr.readCharacter()) != 0x09){
+            while ((ch = terminal.reader().read()) != 0x09){
                 // TAB(0x09)で抜ける
                 char c = (char)ch;
                 System.out.println(String.format("%d, %c", ch, ch));
             }
-        } catch (IOException e) {
+        } catch(IOException e) {
             log.error(e.getMessage(), e);
         }
     }
